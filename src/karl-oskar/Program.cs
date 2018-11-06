5cc3e4f67d10bcea85b25bc6e84dc3f6ab56a0ca
Micke Larsson <micke@miklar.se>
2018-11-06 09:17:04 +0100

    Initiate project with DbUp-PostgreSQL dependency

---
+++ src/karl-oskar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace karl_oskar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
